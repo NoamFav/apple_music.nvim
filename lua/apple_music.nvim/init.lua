2aad89efaf5bb019ade0a8f5c83c4963ac8533b5
noamfav <95364866+NoamFav@users.noreply.github.com>
2025-02-11 16:48:36 +0100

    fixed command

--- lua/apple_music.nvim/init.lua
+++ lua/apple_music.nvim/init.lua
@@ -1,3 +1,5 @@
 local music_ui = require("apple_music.nvim.music_ui")
 
-music_ui.create_music_ui()
+vim.api.nvim_create_user_command("MusicControl", function()
+	music_ui.create_music_ui()
+end, { desc = "Open Music Control UI" })
