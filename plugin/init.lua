6a47c95ace3aea480aed9542d2c7e398d1e2f596
noamfav <95364866+NoamFav@users.noreply.github.com>
2025-02-12 20:19:23 +0100

    more shortcuts

--- plugin/init.lua
+++ plugin/init.lua
@@ -5,16 +5,57 @@ vim.api.nvim_create_user_command("MusicControl", function()
 end, { desc = "Open Music Control UI" })
 
 -- Leader key mapping to call MusicControl
-vim.api.nvim_set_keymap("n", "<Leader>mc", ":MusicControl<CR>", { noremap = true, silent = true })
+-- Open Music Control UI
+vim.api.nvim_set_keymap("n", "<Leader>mu", ":MusicControl<CR>", { noremap = true, silent = true })
+
+-- Play/Pause
 vim.api.nvim_set_keymap(
 	"n",
-	"<Leader>mf",
-	":lua require('apple_music.music_ui').focus_music_ui()<CR>",
+	"<Leader>mp",
+	":lua require('apple_music.music_control').play_pause()<CR>",
+	{ noremap = true, silent = true }
+)
+
+-- Next Track
+vim.api.nvim_set_keymap(
+	"n",
+	"<Leader>mn",
+	":lua require('apple_music.music_control').next_track()<CR>",
+	{ noremap = true, silent = true }
+)
+
+-- Previous Track
+vim.api.nvim_set_keymap(
+	"n",
+	"<Leader>mb",
+	":lua require('apple_music.music_control').previous_track()<CR>",
+	{ noremap = true, silent = true }
+)
+
+-- Volume Up
+vim.api.nvim_set_keymap(
+	"n",
+	"<Leader>m+",
+	":lua require('apple_music.music_control').volume_up()<CR>",
+	{ noremap = true, silent = true }
+)
+
+-- Volume Down
+vim.api.nvim_set_keymap(
+	"n",
+	"<Leader>m-",
+	":lua require('apple_music.music_control').volume_down()<CR>",
 	{ noremap = true, silent = true }
 )
 vim.api.nvim_set_keymap(
 	"n",
 	"<Leader>mq",
 	":lua require('apple_music.music_ui').close_music_ui()<CR>",
+	{ noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+	"n",
+	"<Leader>mf",
+	":lua require('apple_music.music_ui').focus_music_ui()<CR>",
 	{ noremap = true, silent = true }
 )
