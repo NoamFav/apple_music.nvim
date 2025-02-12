7586872991b822844556a767be3fa1880373c5c9
noamfav <95364866+NoamFav@users.noreply.github.com>
2025-02-12 20:57:07 +0100

    slight update again

--- lua/apple_music/music_ui.lua
+++ lua/apple_music/music_ui.lua
@@ -11,7 +11,7 @@ function M.create_music_ui()
 	end
 
 	buf = vim.api.nvim_create_buf(false, true)
-	local width = 47
+	local width = 46
 	local height = 11
 	local row = 1
 	local col = vim.o.columns - width - 2
@@ -63,18 +63,18 @@ function M.create_music_ui()
 			centered_text("   Current Track:"),
 			centered_text(track:gsub("\n", "") or "No track playing"),
 			centered_text(
-				" ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ "
+				" ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ "
 			),
 			centered_text("   Controls:"),
 			centered_text("  h: Previous     p: Play/Pause     l: Next"),
 			centered_text(
-				" ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ "
+				" ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ "
 			),
 			centered_text(" 墳  Volume:"),
 			centered_text(slider),
 			centered_text("-: Volume Down   +: Volume Up   󰖁 : Mute"),
 			centered_text(
-				" ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ "
+				" ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ "
 			),
 			centered_text("   Press 'q' to close."),
 		}
